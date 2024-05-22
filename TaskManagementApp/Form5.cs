a754d75d5da9bb0d27e60afa9d8c7e4f26906404
ok_nabeelah <110231886+oknabeelah@users.noreply.github.com>
2024-05-22 10:50:24 +0100

    //making sure it works
    again but fixed the combined web and app

--- TaskManagementApp/Form5.cs
+++ TaskManagementApp/Form5.cs
@@ -16,12 +16,9 @@ using Microsoft.Win32;
 using System.Collections;
 
 namespace TaskManagementApp
-<<<<<<< Updated upstream
 { //on the github site
-=======
-{
     //making sure it works
->>>>>>> Stashed changes
+    
     public partial class Form5 : Form
     {
         bool sidebarExpand; //declaring 
